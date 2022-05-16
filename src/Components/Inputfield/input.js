b6128bbf9a57e188cadf8f9b2a947ec92d7c14c3
sankjp <santanub65@gmail.com>
2022-05-16 17:23:23 +0530

    setup file structure

---
+++ src/Components/Inputfield/input.js
@@ -0,0 +1,9 @@
+/*import React from "react";
+
+const Input=({type,name,placeholder,value,onchange})=>{
+    return(
+        <input type={type} className={name} placeholder={placeholder} onchange={onchange}/>
+    );
+};
+
+export default Input;*/
